8ca661242688bbf8c7c29de0527aae09bebe414e
atsuminn <a23.ma2zaka@gmail.com>
2018-10-29 18:32:12 +0900

    Create component

--- my-app/src/components/Home.vue
+++ my-app/src/components/Home.vue
@@ -1,76 +1,31 @@
 <template>
   <v-container fluid>
     <v-layout row wrap>
-      <v-flex xs12 class="text-xs-center" mt-5>
+      <!-- <v-flex xs12 class="text-xs-center" mt-5>
         <h1>My Page</h1>
       </v-flex>
       <v-flex xs12 class="text-xs-center" mt-3>
         <p>This is a user's home page</p>
       </v-flex>
-       <v-layout row justify-center>
-    <v-dialog v-model="dialog" persistent max-width="600px">
-      <v-btn slot="activator" color="primary" dark>申請書作成</v-btn>
-      <v-card>
-        <v-card-title>
-          <span class="headline">User Profile</span>
-        </v-card-title>
-        <v-card-text>
-          <v-container grid-list-md>
-            <v-layout wrap>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal first name*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6 md4>
-                <v-text-field
-                  label="Legal last name*"
-                  hint="example of persistent helper text"
-                  persistent-hint
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Email*" required></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field label="Password*" type="password" required></v-text-field>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-select
-                  :items="['0-17', '18-29', '30-54', '54+']"
-                  label="Age*"
-                  required
-                ></v-select>
-              </v-flex>
-              <v-flex xs12 sm6>
-                <v-autocomplete
-                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                  label="Interests"
-                  multiple
-                ></v-autocomplete>
-              </v-flex>
-            </v-layout>
-          </v-container>
-          <small>*indicates required field</small>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+       <v-layout row justify-center> -->
+      
+      <form-dialog />
+      <tab-item />
+
   </v-layout>
-    </v-layout>
   </v-container>
 </template>
 
 <script>
+import TabItem from './TabItem.vue'
+import FormDialog from './FormDialog.vue'
 export default {
-  data: () => ({
-    dialog: false
-  })
+  components: { FormDialog, TabItem },
+  data () {
+    return {
+      dialog: false
+    }
+  }
 }
 </script>
+
---
+++ my-app/src/components/TabItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-toolbar color="cyan" dark tabs>
+
+      <v-toolbar-title>Menu</v-toolbar-title>
+      <v-tabs
+        slot="extension"
+        v-model="tab"
+        color="cyan"
+        align-with-title
+      >
+        <v-tabs-slider color="yellow"></v-tabs-slider>
+
+        <v-tab v-for="item in items" :key="item">
+          {{ item }}
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="item in items" :key="item">
+        <v-card flat>
+          <v-card-text>{{ text }}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tabs-items>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: null,
+      items: [
+        'Task', 'Create Form'
+      ],
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
+</script>
